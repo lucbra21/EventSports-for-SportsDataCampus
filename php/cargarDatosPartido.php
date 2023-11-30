607b95f45c29bb9f379cb5aa576dd54d7cc27ffc
lucbra21 <ing.lucasbracamonte@gmail.com>
2023-11-30 13:39:16 -0300

    cambio forma conexion a la base de datos

--- php/cargarDatosPartido.php
+++ php/cargarDatosPartido.php
@@ -1,13 +1,11 @@
 <?php
 
 //INICIO CONFIGURACION
-
-$host = 'localhost';
-$db   = 'db_sdckl';
-$user = 'root';
-// $pass = 'cGEDeIFNke95';
-$pass = 'root';
-$charset = 'utf8mb4';
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'cGEDeIFNke95');
+// define('DB_PASSWORD', 'root');
+define('DB_NAME', 'db_sdckl');
 
 //FIN CONFIGURACION
 
@@ -16,44 +14,28 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
-// Si es una solicitud OPTIONS (preflight), solo termina con éxito
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit(0);
-}
+$mysqli = new mysqli(DB_HOST, DB_USER , DB_PASSWORD , DB_NAME);
 
-// header('Access-Control-Allow-Origin: https://tudominio.com');
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+if ($mysqli->connect_errno) {
+    echo "Error al conectar con la base de datos ";
+    die();
 }
 
 // Obtener el ID del partido desde la petición
 $matchId = $_GET['matchId'];
 
-// Consulta a la base de datos para obtener datos del partido
-$stmt = $pdo->prepare("SELECT fecha, descripcioncompleta, linkvideocompleto, competidor1, competidor2 FROM vw_grilla_partidos WHERE partido = ?");
 
-$stmt->execute([$matchId]);
-$matchData = $stmt->fetch();
+$sql = "SELECT fecha, descripcioncompleta, linkvideocompleto, competidor1, competidor2 FROM vw_grilla_partidos WHERE partido = " . $matchId;
+$result = $mysqli->query($sql);
+$matchData = $result->fetch_assoc();
 
-// Consulta para obtener jugadores locales
-$stmtLocal = $pdo->prepare("SELECT descripcioncorta, apellido, nombre FROM vw_list_convocatoriaspartido_paracarga WHERE partido = ? AND competidor = ?  ORDER BY apellido, nombre ASC");
-$stmtLocal->execute([$matchId,$matchData['competidor1']]);
-$jugadoresLocales = $stmtLocal->fetchAll(PDO::FETCH_ASSOC); // Suponiendo que quieres todos los registros
+$sql = "SELECT descripcioncorta, apellido, nombre FROM vw_list_convocatoriaspartido_paracarga WHERE partido = ".$matchId." AND competidor = ".$matchData['competidor1']."  ORDER BY apellido, nombre ASC";
+$result = $mysqli->query($sql);
+$jugadoresLocales = $result->fetch_all(MYSQLI_ASSOC);
 
-// Consulta para obtener jugadores visitantes
-$stmtVisitante = $pdo->prepare("SELECT descripcioncorta, apellido, nombre FROM vw_list_convocatoriaspartido_paracarga WHERE partido = ? AND competidor = ?  ORDER BY apellido, nombre ASC");
-$stmtVisitante->execute([$matchId,$matchData['competidor2']]);
-$jugadoresVisitantes = $stmtVisitante->fetchAll(PDO::FETCH_ASSOC); // Suponiendo que quieres todos los registros
+$sql = "SELECT descripcioncorta, apellido, nombre FROM vw_list_convocatoriaspartido_paracarga WHERE partido = ".$matchId." AND competidor = ".$matchData['competidor2']."  ORDER BY apellido, nombre ASC";
+$result = $mysqli->query($sql);
+$jugadoresVisitantes = $result->fetch_all(MYSQLI_ASSOC);
 
 // Añadir las listas de jugadores al objeto que vas a retornar
 $matchData['jugadoresLocales'] = $jugadoresLocales;
